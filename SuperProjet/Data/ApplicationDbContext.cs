8d8a1f9dac6c0501e3a2e4f6d9ffdaaf56490a1d
Jean-Michel Nadeau <jeanmichel.nadeau@gmail.com>
2023-10-19 14:04:35 -0400

    Ajout des conflits

--- SuperProjet/Data/ApplicationDbContext.cs
+++ SuperProjet/Data/ApplicationDbContext.cs
@@ -12,5 +12,6 @@ namespace SuperProjet.Data
         }
         public DbSet<SuperProjet.Models.Trucs> Trucs { get; set; } = default!;
         public DbSet<SuperProjet.Models.Problems> Problems { get; set; } = default!;
+        public DbSet<SuperProjet.Models.Conflit> Conflit { get; set; } = default!;
     }
 }
